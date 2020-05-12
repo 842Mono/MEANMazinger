1fbf3957ea585e4b4dfd479103e94fadeb66993f
842mono <842mono@gmail.com>
2020-05-12 14:03:53 +0200

    beginning to integrate openvidu

--- controller.js
+++ controller.js
@@ -411,7 +411,7 @@ let ControllerFunctions =
         }
       }
     );
-  },
+  }
 }
 
 module.exports = ControllerFunctions;
--- server.js
+++ server.js
@@ -46,9 +46,240 @@ var io = require('socket.io').listen(server);
 //   }
 // });
 server.listen(5100);
-
 //var User = require('./Models/User');
 //var Messages = require('./Models/Messages');
+
+
+var OpenVidu = require('openvidu-node-client').OpenVidu;
+var OpenViduRole = require('openvidu-node-client').OpenViduRole;
+var fs = require('fs');
+var https = require('https');
+
+// Server configuration
+mazinger.use(session({
+  saveUninitialized: true,
+  resave: false,
+  secret: 'MY_SECRET'
+}));
+mazinger.use(express.static(__dirname + '/public_temp_openvidu')); // Set the static files location
+mazinger.use(bodyParser.urlencoded({
+  'extended': 'true'
+})); // Parse application/x-www-form-urlencoded
+mazinger.use(bodyParser.json()); // Parse application/json
+mazinger.use(bodyParser.json({
+  type: 'application/vnd.api+json'
+})); // Parse application/vnd.api+json as json
+
+// Listen (start app with node server.js)
+var options = {
+  key: fs.readFileSync('openvidukey.pem'),
+  cert: fs.readFileSync('openviducert.pem')
+};
+https.createServer(options, mazinger).listen(5000);
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+// Environment variable: URL where our OpenVidu server is listening
+var OPENVIDU_URL = "https://localhost:4443";
+// Environment variable: secret shared with our OpenVidu server
+var OPENVIDU_SECRET = "MY_SECRET";
+
+var OV = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+
+// Collection to pair session names with OpenVidu Session objects
+var mapSessions = {};
+// Collection to pair session names with tokens
+var mapSessionNamesTokens = {};
+
+
+// Mock database
+var users = [{
+  user: "publisher1",
+  pass: "pass",
+  role: OpenViduRole.PUBLISHER
+}, {
+  user: "publisher2",
+  pass: "pass",
+  role: OpenViduRole.PUBLISHER
+}, {
+  user: "subscriber",
+  pass: "pass",
+  role: OpenViduRole.SUBSCRIBER
+}];
+
+/* REST API */
+
+// Login
+mazinger.post('/api-login/login', function (req, res) {
+
+  // Retrieve params from POST body
+  var user = req.body.user;
+  var pass = req.body.pass;
+  console.log("Logging in | {user, pass}={" + user + ", " + pass + "}");
+
+  if (login(user, pass)) { // Correct user-pass
+      // Validate session and return OK 
+      // Value stored in req.session allows us to identify the user in future requests
+      console.log("'" + user + "' has logged in");
+      req.session.loggedUser = user;
+      res.status(200).send();
+  } else { // Wrong user-pass
+      // Invalidate session and return error
+      console.log("'" + user + "' invalid credentials");
+      req.session.destroy();
+      res.status(401).send('User/Pass incorrect');
+  }
+});
+
+// Logout
+mazinger.post('/api-login/logout', function (req, res) {
+  console.log("'" + req.session.loggedUser + "' has logged out");
+  req.session.destroy();
+  res.status(200).send();
+});
+
+// Get token (add new user to session)
+mazinger.post('/api-sessions/get-token', function (req, res) {
+  if (!isLogged(req.session)) {
+      req.session.destroy();
+      res.status(401).send('User not logged');
+  } else {
+      // The video-call to connect
+      var sessionName = req.body.sessionName;
+
+      // Role associated to this user
+      var role = users.find(u => (u.user === req.session.loggedUser)).role;
+
+      // Optional data to be passed to other users when this user connects to the video-call
+      // In this case, a JSON with the value we stored in the req.session object on login
+      var serverData = JSON.stringify({ serverData: req.session.loggedUser });
+
+      console.log("Getting a token | {sessionName}={" + sessionName + "}");
+
+      // Build tokenOptions object with the serverData and the role
+      var tokenOptions = {
+          data: serverData,
+          role: role
+      };
+
+      if (mapSessions[sessionName]) {
+          // Session already exists
+          console.log('Existing session ' + sessionName);
+
+          // Get the existing Session from the collection
+          var mySession = mapSessions[sessionName];
+
+          // Generate a new token asynchronously with the recently created tokenOptions
+          mySession.generateToken(tokenOptions)
+              .then(token => {
+
+                  // Store the new token in the collection of tokens
+                  mapSessionNamesTokens[sessionName].push(token);
+
+                  // Return the token to the client
+                  res.status(200).send({
+                      0: token
+                  });
+              })
+              .catch(error => {
+                  console.error(error);
+              });
+      } else {
+          // New session
+          console.log('New session ' + sessionName);
+
+          // Create a new OpenVidu Session asynchronously
+          OV.createSession()
+              .then(session => {
+                  // Store the new Session in the collection of Sessions
+                  mapSessions[sessionName] = session;
+                  // Store a new empty array in the collection of tokens
+                  mapSessionNamesTokens[sessionName] = [];
+
+                  // Generate a new token asynchronously with the recently created tokenOptions
+                  session.generateToken(tokenOptions)
+                      .then(token => {
+
+                          // Store the new token in the collection of tokens
+                          mapSessionNamesTokens[sessionName].push(token);
+
+                          // Return the Token to the client
+                          res.status(200).send({
+                              0: token
+                          });
+                      })
+                      .catch(error => {
+                          console.error(error);
+                      });
+              })
+              .catch(error => {
+                  console.log("error here!");
+                  console.error(error);
+                  console.log(error);
+                  console.log(JSON.stringify(error.message));
+              });
+      }
+  }
+});
+
+// Remove user from session
+mazinger.post('/api-sessions/remove-user', function (req, res) {
+  if (!isLogged(req.session)) {
+      req.session.destroy();
+      res.status(401).send('User not logged');
+  } else {
+      // Retrieve params from POST body
+      var sessionName = req.body.sessionName;
+      var token = req.body.token;
+      console.log('Removing user | {sessionName, token}={' + sessionName + ', ' + token + '}');
+
+      // If the session exists
+      if (mapSessions[sessionName] && mapSessionNamesTokens[sessionName]) {
+          var tokens = mapSessionNamesTokens[sessionName];
+          var index = tokens.indexOf(token);
+
+          // If the token exists
+          if (index !== -1) {
+              // Token removed
+              tokens.splice(index, 1);
+              console.log(sessionName + ': ' + tokens.toString());
+          } else {
+              var msg = 'Problems in the app server: the TOKEN wasn\'t valid';
+              console.log(msg);
+              res.status(500).send(msg);
+          }
+          if (tokens.length == 0) {
+              // Last user left: session must be removed
+              console.log(sessionName + ' empty!');
+              delete mapSessions[sessionName];
+          }
+          res.status(200).send();
+      } else {
+          var msg = 'Problems in the app server: the SESSION does not exist';
+          console.log(msg);
+          res.status(500).send(msg);
+      }
+  }
+});
+
+/* REST API */
+
+
+
+/* AUXILIARY METHODS */
+
+function login(user, pass) {
+  return (users.find(u => (u.user === user) && (u.pass === pass)));
+}
+
+function isLogged(session) {
+  return (session.loggedUser != null);
+}
+
+function getBasicAuth() {
+  return 'Basic ' + (new Buffer('OPENVIDUAPP:' + OPENVIDU_SECRET).toString('base64'));
+}
+
+/* AUXILIARY METHODS */
 
 
 
